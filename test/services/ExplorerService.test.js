82187be835aa85d2b4e7bc6360d285d2c35bc09f
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-04-26 12:49:51 -0500

    Agregando pruebas de ExplorerService

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,21 @@
+const ExplorerService = require('./../../lib/services/ExplorerService');
+
+describe("Test para ExplorerServices", () => {
+  test("1) Calcular todos los explorers en una misión", () => {
+    const explorers = [{mission: "node"}];
+    const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+    expect(explorersInNode.length).toBe(1);
+  })
+  
+  test("2) Calcular cuantos explorers por misión", () => {
+    const explorers = [{mission: "node"}, {mission: "node"}, {mission: "java"}];
+    const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+    expect(explorersInNode).toBe(2);
+  })
+
+  test("3) Obtener el username de los explores por misión", () => {
+    const explorers = [{username: 'explorer1' ,mission: "java"}];
+    const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+    expect(explorersInNode.username).toBe(explorers.username);
+  })
+})
